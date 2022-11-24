d5126601af8c5f0ab7c65b77755a0f0a52d9d34b
Maxim <the.mlex@gmail.com>
2022-11-24 11:45:58 +0300

    travis-ci

---
+++ test.php
@@ -0,0 +1,14 @@
+#!/usr/bin/env php
+<?php
+error_reporting(E_ALL);
+print_r($_SERVER);
+ini_set("trochilidae.server", "127.0.0.1");
+ini_set("trochilidae.port", "30001");
+echo 'trochilidae.server: ', ini_get('trochilidae.server'), PHP_EOL;
+echo 'trochilidae.port: ', ini_get('trochilidae.port'), PHP_EOL;
+
+trochilidae_set_tag("controller", "testController");
+trochilidae_set_tag("action", "testAction");
+trochilidae_set_tag("company", "testCompany");
+
+echo 'OK', PHP_EOL;
